b36c47a9e0663a43b95e098df402b04b50908d6c
valium69mg <xdeus.v2@gmail.com>
2026-02-18 10:02:58 -0600

    feat: agregar implementación de logout a la aplicación

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -4,6 +4,7 @@ import type { LoginResponse } from "../api/api";
 export interface UserContextType {
   user: LoginResponse | null;
   setUser: (user: LoginResponse | null) => void;
+  logout: () => void;
 }
 
 export const UserContext = createContext<UserContextType | undefined>(undefined);
--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -8,10 +8,15 @@ export function UserProvider({ children }: { children: ReactNode }) {
     const saved = localStorage.getItem("loginData");
     return saved ? JSON.parse(saved) : null;
   });
+
+  const logout = () => {
+    localStorage.removeItem("loginData");
+    setUser(null);
+  };
+
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {" "}
-      {children}{" "}
+    <UserContext.Provider value={{ user, setUser, logout }}>
+      {children}
     </UserContext.Provider>
   );
 }
