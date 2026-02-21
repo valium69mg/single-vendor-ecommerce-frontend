c789e200d68c551a6fbd8d8cd471551241efd184
valium69mg <xdeus.v2@gmail.com>
2026-02-21 16:00:20 -0600

    feat: agregar traducciones a AdminSideBar

--- src/components/admin/AdminSideBar.tsx
+++ src/components/admin/AdminSideBar.tsx
@@ -18,30 +18,34 @@ import {
 } from "lucide-react";
 import SideBarItem from "../common/SideBarItem";
 import SideBarAccordion from "../common/SideBarAccordion";
+import { useTranslation } from "react-i18next";
 
-const productOptions = [
-  {
-    id: "products",
-    name: "Products",
-    icon: <Box className="h-4 w-4 mr-2" />,
-  },
-  {
-    id: "categories",
-    name: "Categories",
-    icon: <Tags className="h-4 w-4 mr-2" />,
-  },
-  {
-    id: "brands",
-    name: "Brands",
-    icon: <Layers className="h-4 w-4 mr-2" />,
-  },
-  {
-    id: "materials",
-    name: "Materials",
-    icon: <Package className="h-4 w-4 mr-2" />,
-  },
-];
 export default function AdminSideBar() {
+  const { t } = useTranslation();
+
+  const productOptions = [
+    {
+      id: "products",
+      name: t("products"),
+      icon: <Box className="h-4 w-4 mr-2" />,
+    },
+    {
+      id: "categories",
+      name: t("categories"),
+      icon: <Tags className="h-4 w-4 mr-2" />,
+    },
+    {
+      id: "brands",
+      name: t("brands"),
+      icon: <Layers className="h-4 w-4 mr-2" />,
+    },
+    {
+      id: "materials",
+      name: t("materials"),
+      icon: <Package className="h-4 w-4 mr-2" />,
+    },
+  ];
+
   return (
     <Sidebar className="flex flex-col h-full">
       {/* Header */}
@@ -52,35 +56,35 @@ export default function AdminSideBar() {
         <SidebarGroupContent>
           {/* Dashboard */}
           <SideBarItem
-            title="Dashboard"
+            title={t("dashboard")}
             href="/admin/dashboard"
             icon={<LayoutDashboard className="h-4 w-4" />}
           />
 
           {/* Products Accordion */}
           <SideBarAccordion
-            title="Products"
+            title={t("products")}
             icon={<Package className="h-4 w-4" />}
             options={productOptions}
           />
 
           {/* Orders */}
           <SideBarItem
-            title="Orders"
+            title={t("orders")}
             href="/admin/orders"
             icon={<ShoppingCart className="h-4 w-4" />}
           />
 
           {/* Users */}
           <SideBarItem
-            title="Users"
+            title={t("users")}
             href="/admin/users"
             icon={<Users className="h-4 w-4" />}
           />
         </SidebarGroupContent>
       </SidebarGroup>
 
-      {/* Footer (sticks to bottom) */}
+      {/* Footer */}
       <AdminSideBarFooter />
     </Sidebar>
   );
