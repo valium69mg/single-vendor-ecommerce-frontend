2e931274227ba6584daf374e5fa04a4261a17ecc
carlostr <xdeus.v2@gmail.com>
2026-04-06 08:11:37 -0600

    fix eliminar useNavigate sin usar en Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import NavbarLogo from "./NavbarLogo";
 import NavbarCategories from "./NavbarCategories";
 import NavbarSearch from "./NavbarSearch";
 import NavbarProfile from "./NavbarProfile";
 
 export default function Navbar() {
-  const navigate = useNavigate();
-
   return (
     <div className="flex justify-center w-full sticky top-0 z-50">
       <div className="grid grid-cols-[1fr_2fr_1fr] items-center h-16 w-full px-4 md:px-6 lg:px-8">
